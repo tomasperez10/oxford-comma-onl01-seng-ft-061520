e50a7016f4f2667f4dff5bd03fe1c8626bb7bbfb
Tomas Perez <tomasitopg6@gmail.com>
2020-06-18 02:27:11 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,6 +5,6 @@ if array.size == 1
 elsif array.size == 2
   return array.join(" and ")
 else
-  return array[0..-2].join(", ") + ", and"
+  return array[0..-2].join(", ") + ", and " + array[-1]
 
 end
